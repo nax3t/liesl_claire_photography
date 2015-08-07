54c03b0a2906397e1a21a88788c3df6e18306d28
ian schoonover <iantskon@gmail.com>
2015-08-07 15:15:11 +0200

    Add post logic

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,55 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
+    if current_user.admin
+      @post = Post.new
+    else
+      redirect_to root_path
+      flash.alert = "Access denied!"
+    end
   end
 
   def create
+    @post = current_user.posts.build post_params
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
   end
 
   def show
+    @post = Post.find params[:id]
   end
 
   def edit
+    @post = Post.find params[:id]
   end
 
   def update
+    @post = Post.find params[:id]
+    if @post.update post_params
+      redirect_to @post
+    else
+      render :edit
+    end
   end
 
   def destroy
+    if current_user.admin
+      @post = Post.find params[:id]
+      @post.destroy
+    else
+      redirect_to root_path
+      flash.notice = "Access denied!"
+    end
   end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
 end
---
+++ db/migrate/20150807131456_add_use_r_id_to_posts.rb
@@ -0,0 +1,5 @@
+class AddUseRIdToPosts < ActiveRecord::Migration
+  def change
+    add_column :posts, :user_id, :integer
+  end
+end
